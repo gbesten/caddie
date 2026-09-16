e3587c3d0dce24b1eecbe7feac6c083e8d805c6c
gbesten <148280873+gbesten@users.noreply.github.com>
2026-09-16 21:18:22 +0200

    Mon premier commit

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'caddie-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).then(r => {
+    if (new URL(e.request.url).origin === location.origin) { const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); }
+    return r;
+  }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html'))));
+});
